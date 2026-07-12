bd8a3ad852227d323e85fe0ef4537833bac9855d
zhutra <zhutra@users.noreply.github.com>
2026-07-12 12:59:48 +0200

    build(ayastorm): update build script for VS180 and local paths

--- build_ayastorm.bat
+++ build_ayastorm.bat
@@ -1,30 +1,34 @@
 @echo off
 set PYTHONUTF8=1
-set AUTOBUILD_VSVER=170
-set AUTOBUILD_VARIABLES_FILE=c:\firestorm\fs-build-variables\variables
+set AUTOBUILD_VSVER=180
+set AUTOBUILD_WIN_VSTOOLSET=v145
+set AUTOBUILD_VARIABLES_FILE=C:\work_ayastorm\fs-build-variables\variables
 set AUTOBUILD_CONFIG_FILE=my_autobuild.xml
 set PATH=C:\cygwin64\bin;%PATH%
 
-cd c:\firestorm\phoenix-firestorm
+cd C:\work_ayastorm\phoenix-firestorm
 
-echo [1] 特殊文字を修正中...
+echo [1] Modifying special characters...
 python fixall2.py
 
-echo [2] Configure中...
-autobuild configure -A 64 -c ReleaseFS_open -- --fmodstudio -DLL_TESTS:BOOL=FALSE --package --chan Ayastorm-release
+echo [2] Deleting previous build...
+rmdir /s /q build-vc180-64
+
+echo [3] Configuring...
+autobuild configure -A 64 -c ReleaseFS_open -- --fmodstudio -DLL_TESTS:BOOL=FALSE --package --chan AYAstorm-release
 if errorlevel 1 (
     echo Configure失敗
     pause
     exit /b 1
 )
 
-echo [3] ビルド中...
+echo [4] Building...
 autobuild build -A 64 -c ReleaseFS_open --no-configure
 if errorlevel 1 (
-    echo ビルド失敗
+    echo Build failed!
     pause
     exit /b 1
 )
 
-echo 完了
+echo Complete!
 pause
