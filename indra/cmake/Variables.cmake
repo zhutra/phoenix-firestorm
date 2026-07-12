4b4e2ffcbaad4b2050881c7c4f4dbc5be832b1c7
zhutra <zhutra@users.noreply.github.com>
2026-07-12 12:59:49 +0200

    chore(cmake): update Discord API key

--- indra/cmake/Variables.cmake
+++ indra/cmake/Variables.cmake
@@ -184,7 +184,7 @@ set(FLICKR_API_KEY "daaabff93a967e0f37fa18863bb43b29")
 set(FLICKR_API_SECRET "846f0958020b553e") 
 
 # Discord client key.
-set(DDISCORD_API_KEY "427641535253708801")
+set(DDISCORD_API_KEY "1394782217405862001")
 
 # FS:ND Don't force this into the cache, that can have some strange effects. Instead make it a normal variable
 #set(ENABLE_SIGNING OFF CACHE BOOL "Enable signing the viewer")
